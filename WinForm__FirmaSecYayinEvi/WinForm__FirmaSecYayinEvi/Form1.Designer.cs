7e2741e412f995836e6460a59642d6ae648759b1
omer.kaplan <omer.kaplan@surat.com.tr>
2016-05-30 17:33:41 +0300

    proje eklendi

---
+++ WinForm__FirmaSecYayinEvi/WinForm__FirmaSecYayinEvi/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace WinForm__FirmaSecYayinEvi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(776, 446);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(776, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(776, 446);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ WinForm__FirmaSecYayinEvi/WinForm__FirmaSecYayinEvi/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForm__FirmaSecYayinEvi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+}
+
+
+//using System;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Data;
+//using System.Drawing;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Windows.Forms;
+
+//namespace WindowsFormsApplication1
+//{
+//    public partial class Form1 : Form
+//    {
+//        public Form1()
+//        {
+//            InitializeComponent();
+//        }
+
+//        private void Form1_Load(object sender, EventArgs e)
+//        {
+
+        
+//        }
+
+//        private void webBrowser1_Navigating(object sender, WebBrowserNavigatingEventArgs e)
+//        {
+//            // Set text while the page has not yet loaded.
+//            this.Text = "Navigating";
+//        }
+
+//        private void webBrowser1_DocumentCompleted(object sender,
+//            WebBrowserDocumentCompletedEventArgs e)
+//        {
+//            // Better use the e parameter to get the url.
+//            // ... This makes the method more generic and reusable.
+//            this.Text = e.Url.ToString() + " loaded";
+//        }
+
+//        private void webBrowser1_DocumentCompleted_1(object sender, WebBrowserDocumentCompletedEventArgs e)
+//        {
+
+//        }
+
+//        private void textBox1_TextChanged(object sender, EventArgs e)
+//        {
+            
+//        }
+
+//        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+//        {
+//            if (e.KeyCode == Keys.Enter)
+//            {
+//                if(textBox1.Text.Length ==0)
+//                    textBox1.Text = "http://www.firmasec.com/firma/ara/yay%C4%B1nevi/istanbul/1";
+                                
+//                //webBrowser1.Navigate("http://www.firmasec.com/firma/ara/yay%C4%B1nevi/istanbul/1");
+//                webBrowser1.Navigate(textBox1.Text);
+//            }
+//        }
+
+
+//    }
+//}
+
